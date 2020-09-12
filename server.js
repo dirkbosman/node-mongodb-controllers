8863cd9109f73351e3b0f8cf58a768f8709cbf8f
Dirk Bosman <dirkjobosman@gmail.com>
2020-09-12 14:34:32 +0200

    client-server connection

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-require('dotenv').config()
-require('colors');
+require("dotenv").config();
+require("colors");
 const express = require("express");
+const cors = require("cors");
 
-const connectDB = require('./dbinit');
-const users = require('./api/users');
-const orders = require('./api/orders');
-const errorHandler = require('./middleware/error');
+const connectDB = require("./dbinit");
+const users = require("./api/users");
+const orders = require("./api/orders");
+const errorHandler = require("./middleware/error");
 
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
 connectDB();
 
 app.use(express.json());
-app.use('/users', users);
-app.use('/orders', orders);
+app.use(cors());
+
+app.use("/users", users);
+app.use("/orders", orders);
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Started server on port ${PORT}`.rainbow.bold.inverse));
-
+app.listen(PORT, () =>
+  console.log(`Started server on port ${PORT}`.rainbow.bold.inverse)
+);
